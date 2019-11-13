0599d44d0e32328b161a02b7d98b1e165fcabcc9
jianrongdeng <jdeng@Dengs-MacBook-Air.local>
2019-11-13 14:55:43 +0800

    Latest codes on pickle data and muon track linear fit

---
+++ ana/scripts/EventDisplay/pickle_pixels.py
@@ -0,0 +1,13 @@
+"""
+============================
+pickle_pixels.py
+============================
+
+	date: 20190806 by Jianrong Deng
+	purpose:
+		pickle a list of candidate pixels to a binary .dat file 
+                use pickle.dump and pickle.load for write/read data
+                to/from files. 
+============================
+"""
+
